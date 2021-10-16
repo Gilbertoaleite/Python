4b925957a34e6c4137a105516e16faaf1e426515
GILBERTO DE ARAUJO LEITE <90141253+Gilbertoaleite@users.noreply.github.com>
2021-10-16 10:30:31 -0300

    Ediarista.md
    Api do projeto de estudo ediarista
    deploy da app

--- ediaristas_workshop/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import DiaristasCidadeList
-
-urlpatterns = [
-    path('diaristas-cidade', DiaristasCidadeList.as_view(), name='diaristas-cidade-list'),
-]
